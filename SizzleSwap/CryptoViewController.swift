45b121ea45a9ba7cffab06ae63e7b41552be539c
Zwe Htin Aung <96027995+Zwe-Htin-Aung-Codisto@users.noreply.github.com>
2022-05-13 14:24:13 +1000

    Initial UI Layouting Currency and Crypto Features

---
+++ SizzleSwap/CryptoViewController.swift
@@ -0,0 +1,18 @@
+//
+//  File.swift
+//  SizzleSwap
+//
+//  Created by Zwe Htin Aung on 13/5/2022.
+//
+
+import UIKit
+
+class CryptoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
---
+++ SizzleSwap/CurrencyViewController.swift
@@ -0,0 +1,18 @@
+//
+//  File.swift
+//  SizzleSwap
+//
+//  Created by Zwe Htin Aung on 13/5/2022.
+//
+
+import UIKit
+
+class CurrencyViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
