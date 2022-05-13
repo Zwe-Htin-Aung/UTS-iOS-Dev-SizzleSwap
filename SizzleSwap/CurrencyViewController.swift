345530a9747475292190341b555dea8c05997c51
Zwe Htin Aung <96027995+Zwe-Htin-Aung-Codisto@users.noreply.github.com>
2022-05-13 23:09:17 +1000

    Add Currency Swap UI and Initial API Call with JSON Decoding

--- SizzleSwap/CurrencyViewController.swift
+++ SizzleSwap/CurrencyViewController.swift
@@ -5,14 +5,20 @@
 //  Created by Zwe Htin Aung on 13/5/2022.
 //
 
+import Foundation
 import UIKit
 
 class CurrencyViewController: UIViewController {
+    
+    var currencyCodeRequest = CurrencyCodeRequest();
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
-
-
+    
+    @IBAction func convertPressed(_ sender: UIButton) {
+        currencyCodeRequest.fetch()
+    }
+    
 }
