f8b85beca9ec95b24cbecbfe342043fa07cc3192
Zwe Htin Aung <96027995+Zwe-Htin-Aung-Codisto@users.noreply.github.com>
2022-05-18 22:53:27 +1000

    Make Dropdown Lists functional in CurrencySwap

--- SizzleSwap/CurrencySwap/Model/CurrencyCodeRequest.swift
+++ SizzleSwap/CurrencySwap/Model/CurrencyCodeRequest.swift
@@ -10,26 +10,31 @@ import Foundation
 struct CurrencyCodeRequest {
     
     let apiKey = "XF7tAgubdBbD5VQr0BBCYjfUiJhXyDyV"
-    let currencyCodeURL =  "https://api.apilayer.com/exchangerates_data/symbols";
+    let currencyCodeURL =  "https://api.apilayer.com/exchangerates_data/symbols"
+    // var result : CurrencyCodes
     
-    func fetch () {
+    func fetch() -> CurrencyCodes {
         let semaphore = DispatchSemaphore (value: 0)
+        var result : CurrencyCodes = CurrencyCodes(success: false, symbols: [:])
         var request = URLRequest(url: URL(string: currencyCodeURL)!,timeoutInterval: Double.infinity)
         request.httpMethod = "GET"
         request.addValue(apiKey, forHTTPHeaderField: "apikey")
-        let task = URLSession.shared.dataTask(with: request) { data, response, error in
-          guard let data = data else {
-              print(String(describing: error))
-              return
-          }
-            decodeJSON(currencyData: data)
-            semaphore.signal()
+        let urlSession = URLSession(configuration: .default)
+        let task = urlSession.dataTask(with: request) { (data, response, error) in
+            if let safeData = data {
+                result = decodeJSON(currencyData: safeData)
+                semaphore.signal()
+            } else {
+                print(String(describing: error))
+                return
+            }
         }
         task.resume()
         semaphore.wait()
+        return result
     }
     
-    func decodeJSON (currencyData : Data) {
+    func decodeJSON(currencyData : Data) -> CurrencyCodes {
         let decoder = JSONDecoder()
         do {
             let currencyCodes = try decoder.decode(CurrencyCodes.self , from: currencyData)
@@ -37,9 +42,10 @@ struct CurrencyCodeRequest {
             for (code, currencyName) in currencyCodes.symbols {
                 print("Code: \(code) --> Currency: \(currencyName)")
             }
-            
+            return currencyCodes
         } catch {
-            print(error);
+            print(error)
+            return CurrencyCodes(success: false, symbols: [:])
         }
     }
 }
